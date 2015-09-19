95def6e1c0dd0d7724a15e2e54243906dfaa8df8
jkurzer <jake.kurzer@gmail.com>
2015-09-19 00:40:09 -0400

    Initial Commit For Real

---
+++ Assets/CameraTrack.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTrack : MonoBehaviour {
+
+	public Transform player;
+	// Use this for initialization
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	private Camera thisC;
+
+	void Start ()
+	{
+		thisC = GetComponent<Camera>();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if (player)
+		{
+			Vector3 point = thisC.WorldToViewportPoint(player.position);
+			Vector3 delta = player.position - thisC.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); 
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+	}
+
+}
