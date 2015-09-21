1cf7fdfad0a6de0f6790d611a12897ea967b5641
jkurzer <jake.kurzer@gmail.com>
2015-09-20 23:24:05 -0400

    Enemies and shields
    Step 0 of four!

---
+++ Assets/ShieldHit.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldHit : MonoBehaviour {
+
+	private float scale = 1;
+	private Vector3 sSm;
+	private float sRcc; //
+	private CapsuleCollider refT;
+	void Awake()
+	{
+		sSm = this.gameObject.transform.localScale;
+		sSm = new Vector3 (sSm.x, sSm.y, sSm.z);
+		refT = this.GetComponent<CapsuleCollider> ();
+		sRcc = refT.radius;
+	}
+
+	void OnTriggerEnter ( Collider other)
+	{
+		if (other.tag == "shot") {
+			Destroy (other.gameObject);
+			scale -= 0.05f;
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		scale += (0.06f * Time.deltaTime);
+		scale = Mathf.Min (scale, 1.1f);
+		this.gameObject.transform.localScale = sSm * scale;
+		refT.radius = sRcc * scale;
+	}
+}
---
+++ Assets/destroySlightlyAfterHit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroySlightlyAfterHit : MonoBehaviour {
+	
+	void OnTriggerEnter ( Collider other)
+	{
+		Transform v = other.gameObject.transform.parent;
+		if( !other.tag.Contains("shot"))
+			if( (v == null && other.tag != "Player") 
+			   || (v != null && v.gameObject.tag != "Player"))
+				Destroy (this.gameObject, 1.1f * Time.smoothDeltaTime);
+	}
+}
